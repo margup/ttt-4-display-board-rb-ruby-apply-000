b30212e13327c946d9c828806b7f12964797c111
bright-lifecycle-9875 <mairgup@icloud.com>
2020-08-27 18:52:32 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(ttt)
+  puts " #{ttt[0]} | #{ttt[1]} | #{ttt[2]} "
+  puts "-----------"
+  puts " #{ttt[3]} | #{ttt[4]} | #{ttt[5]} "
+  puts "-----------"
+  puts " #{ttt[6]} | #{ttt[7]} | #{ttt[8]} " 
+end
+board = [" "," "," "," ","x"," "," "," "," "]
+display_board(board)
